d8c9d67ec3d1a2d06b936dfbbabbb56c6fc5a0f0
marselester <marselester@ya.ru>
2013-12-09 16:27:22 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from distutils.core import setup
+
+
+setup(
+    name='Flask-API-Utils',
+    version='0.1.0-dev',
+    packages=['api_utils'],
+    author='Marsel Mavletkulov',
+    author_email='marselester@ya.ru',
+    description='Flask utils which help you to create API.',
+    long_description=open('README.rst').read(),
+    install_requires=['Flask>=0.10'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
