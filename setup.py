c9a61455540b3753dc096a1dd2dcd588ba76988a
marselester <marselester@ya.ru>
2014-06-21 20:29:40 +0700

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='Flask-API-Utils',
-    version='0.2.0',
+    version='1.0.0',
     packages=['api_utils'],
     author='Marsel Mavletkulov',
     author_email='marselester@ya.ru',
