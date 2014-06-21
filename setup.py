39a834dff1296a3dca8f4a8ddd715701581563f2
marselester <marselester@ya.ru>
2014-06-21 15:49:30 +0700

    Update classifiers of setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from distutils.core import setup
 
-
 setup(
     name='Flask-API-Utils',
     version='0.2.0',
@@ -9,14 +8,16 @@ setup(
     author='Marsel Mavletkulov',
     author_email='marselester@ya.ru',
     url='https://github.com/marselester/flask-api-utils',
-    description='Flask utils which help you to create API.',
+    description='Flask-API-Utils helps you to create APIs.',
     long_description=open('README.rst').read(),
     install_requires=['Flask>=0.10'],
     classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
